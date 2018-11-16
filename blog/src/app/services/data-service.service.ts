e4266ebc53309bf3452819646b30d5d3d83ce9f6
kKapalka <k.kapalka@hotmail.com>
2018-11-16 16:00:47 +0100

    Kolejne zadania

--- blog/src/app/services/data-service.service.ts
+++ blog/src/app/services/data-service.service.ts
@@ -11,7 +11,7 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getAll() {
-    return this.http.get(this.url + '/photos')
+    return this.http.get(this.url + '/api/posts')
       .pipe(
         map( (x: any[]) => x.slice(0, 5))
       );
